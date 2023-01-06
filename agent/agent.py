56d8304a61893b72e54eb3383199704705b42a06
Fuyu Linh <nhnlinh20@apcs.fitus.edu.vn>
2023-01-06 19:16:18 +0700

    commit

--- agent/agent.py
+++ agent/agent.py
@@ -8,255 +8,198 @@ import numpy as np
 class Agent:
     __hint = []
     __coordinate = Position(None, None)
-    __agent_map = [] 
+    __agent_map = []
+    __map = Map(None, None, None, None, None)
+    __teleport = True
     # Map that agent can see (0 for any tile that has not been discovered, 
     # -1 for those that has bee discovered)
 
-    def __init__(self, coordinate, hint, agent_map):
-        self.__coordinate = coordinate
-        self.__hint = hint
-        self.__agent_map = agent_map
+    def __init__(self, map):
+        self.__map = map
 
-    def init_map(self, width, height):
-        for x in range(height):
+    def get_agent_map(self):
+        return self.__agent_map
+
+    def get_coordinate(self):
+        return self.__coordinate
+
+    def init_map(self):
+        for x in range(self.__map.get_height()):
             row = []
-            for y in range(width):
-                row.append('0')
+            for y in range(self.__map.get_width()):
+                row.append('1')
             self.__agent_map.append(row)
 
-    def spawn(self, width, height, map):
+    def spawn(self):
+        map = self.__map.get_data()
         while True:
-            x = random.randint(0, height - 1)
-            y = random.randint(0, width - 1)
+            x = random.randint(0, self.__map.get_height() - 1)
+            y = random.randint(0, self.__map.get_width() - 1)
             if ('M' not in map[x][y] and 'P' not in map[x][y] and map[x][y] != '0'):
-                self.__agent_map[x][y].append('-1')
+                self.__agent_map[x][y] = '-'
                 return self.__coordinate.set(x, y)
 
-    def action(self, map):
-        random_action = random.randint(0, 3)
-        if (random_action == 0): # verify if the hint is true or not
-            in_progress
-        elif (random_action == 1): # move 1-2 steps and perform small scan
-            move = random.choice([1, 2])
+
+    def __move(self, direction, step):
+        map = self.__map.get_data()
+        x = self.__coordinate.get_x()
+        y = self.__coordinate.get_y()
+        while (step > 0):
+            if (direction == 0): # Up
+                if ('M' in map[x-1][y] or map[x-1][y] == '0' or x == 0):
+                    break
+                x = x - 1
+                self.__agent_map[x][y] = '-'
+                step = step - 1
+            elif (direction == 1): # Down
+                if ('M' in map[x+1][y] or map[x+1][y] == '0' or x == self.__map.get_height() - 1):
+                    break
+                x = x + 1
+                self.__agent_map[x][y] = '-'
+                step = step - 1
+            elif (direction == 2): # Left
+                if ('M' in map[x][y-1] or map[x][y-1] == '0' or y == 0):
+                    break
+                y = y - 1
+                self.__agent_map[x][y] = '-'
+                step = step - 1
+            elif (direction == 3): # Right
+                if ('M' in map[x][y+1] or map[x][y+1] == '0' or y == self.__map.get_width() - 1):
+                    break
+                y = y + 1
+                self.__agent_map[x][y] = '-'
+                step = step - 1
+        self.__coordinate.set(x, y)
+
+    def __scan(self, type):
+        x = self.__coordinate.get_x()
+        y = self.__coordinate.get_y()
+        for i in range(x - type, x + type + 1):
+            for j in range(y - type, y + type + 1):
+                if i < 0 or j < 0 or i > self.__map.get_height() - 1 or j > self.__map.get_width() - 1:
+                    continue
+                self.__agent_map[i][j] = '-'
+    
+    def __calculate_point(self, x, y):
+        sum_point = 0
+        for i in range(x - 2, x + 3):
+            for j in range(y - 2, y + 3):
+                if i < 0 or j < 0 or i > self.__map.get_height() - 1 or j > self.__map.get_width() - 1:
+                    continue
+                if (self.__agent_map[i][j] == '1'):
+                    sum_point += 1
+        return sum_point
+
+    def __choose_action(self):
+        map = self.__map.get_data()
+        max_count = 0
+        current_count = 0 
+        step = 0
+        recored_dir = 0
+        x = self.__coordinate.get_x()
+        y = self.__coordinate.get_y()
+        if self.__calculate_point(x, y) > 5:
+            return max_count, step, recored_dir
+        for move in range(1, 5):
+            # Move up
             x = self.__coordinate.get_x()
             y = self.__coordinate.get_y()
-            while True:
-                step = random.randint(0, 3)
-                if (step == 0):
-                    x = x + move
-                    if (map[x][y] == '0' or 'M' in map[x][y]):
-                        continue
-                    if ('T' in map[x - 1][y - 1]):
-                        return self.__coordinate.set(x - 1, y - 1)
-                    if ('T' in map[x - 1][y]):
-                        return self.__coordinate.set(x - 1, y)
-                    if ('T' in map[x + 1][y + 1]):
-                        return self.__coordinate.set(x + 1, y + 1)
-                    if ('T' in map[x][y - 1]):
-                        return self.__coordinate.set(x, y - 1)
-                    if ('T' in map[x][y + 1]):
-                        return self.__coordinate.set(x, y + 1)
-                    if ('T' in map[x + 1][y - 1]):
-                        return self.__coordinate.set(x + 1, y - 1)
-                    if ('T' in map[x + 1][y]):
-                        return self.__coordinate.set(x + 1, y)
-                    if ('T' in map[x + 1][y + 1]):
-                        return self.__coordinate.set(x + 1, y + 1)
-                    self.append_map(0, x, y)
-                    return self.__coordinate.set(x, y)
-                elif (step == 1):
-                    x = x - move
-                    if (map[x][y] == '0' or 'M' in map[x][y]):
-                        continue
-                    if ('T' in map[x - 1][y - 1]):
-                        return self.__coordinate.set(x - 1, y - 1)
-                    if ('T' in map[x - 1][y]):
-                        return self.__coordinate.set(x - 1, y)
-                    if ('T' in map[x + 1][y + 1]):
-                        return self.__coordinate.set(x + 1, y + 1)
-                    if ('T' in map[x][y - 1]):
-                        return self.__coordinate.set(x, y - 1)
-                    if ('T' in map[x][y + 1]):
-                        return self.__coordinate.set(x, y + 1)
-                    if ('T' in map[x + 1][y - 1]):
-                        return self.__coordinate.set(x + 1, y - 1)
-                    if ('T' in map[x + 1][y]):
-                        return self.__coordinate.set(x + 1, y)
-                    if ('T' in map[x + 1][y + 1]):
-                        return self.__coordinate.set(x + 1, y + 1)
-                    self.append_map(0, x, y)
-                    return self.__coordinate.set(x, y)
-                elif (step == 2):
-                    y = y + move
-                    if (map[x][y] == '0' or 'M' in map[x][y]):
-                        continue
-                    if ('T' in map[x - 1][y - 1]):
-                        return self.__coordinate.set(x - 1, y - 1)
-                    if ('T' in map[x - 1][y]):
-                        return self.__coordinate.set(x - 1, y)
-                    if ('T' in map[x + 1][y + 1]):
-                        return self.__coordinate.set(x + 1, y + 1)
-                    if ('T' in map[x][y - 1]):
-                        return self.__coordinate.set(x, y - 1)
-                    if ('T' in map[x][y + 1]):
-                        return self.__coordinate.set(x, y + 1)
-                    if ('T' in map[x + 1][y - 1]):
-                        return self.__coordinate.set(x + 1, y - 1)
-                    if ('T' in map[x + 1][y]):
-                        return self.__coordinate.set(x + 1, y)
-                    if ('T' in map[x + 1][y + 1]):
-                        return self.__coordinate.set(x + 1, y + 1)
-                    self.append_map(0, x, y)
-                    return self.__coordinate.set(x, y)
-                elif (step == 3):
-                    y = y - move
-                    if (map[x][y] == '0' or 'M' in map[x][y]):
-                        continue
-                    if ('T' in map[x - 1][y - 1]):
-                        return self.__coordinate.set(x - 1, y - 1)
-                    if ('T' in map[x - 1][y]):
-                        return self.__coordinate.set(x - 1, y)
-                    if ('T' in map[x + 1][y + 1]):
-                        return self.__coordinate.set(x + 1, y + 1)
-                    if ('T' in map[x][y - 1]):
-                        return self.__coordinate.set(x, y - 1)
-                    if ('T' in map[x][y + 1]):
-                        return self.__coordinate.set(x, y + 1)
-                    if ('T' in map[x + 1][y - 1]):
-                        return self.__coordinate.set(x + 1, y - 1)
-                    if ('T' in map[x + 1][y]):
-                        return self.__coordinate.set(x + 1, y)
-                    if ('T' in map[x + 1][y + 1]):
-                        return self.__coordinate.set(x + 1, y + 1)
-                    self.append_map(0, x, y)
-                    return self.__coordinate.set(x, y)
-        elif (random_action == 2): # move 3-4 steps
-            move = random.choice([3, 4])
+            x -= move
+            current_count = self.__calculate_point(x, y)
+            if x < 0 or y < 0 or x > self.__map.get_height() - 1 or y > self.__map.get_width() - 1 or 'P' in map[x][y] or 'M' in map[x][y] or map[x][y] == '0':
+                break
+            if (current_count>max_count):
+                step = move
+                max_count = current_count
+                recored_dir = 1
+
+        for move in range(1, 5):    
+            # Move down
             x = self.__coordinate.get_x()
             y = self.__coordinate.get_y()
-            while True:
-                step = random.randint(0, 3)
-                if (step == 0):
-                    x = x + move
-                    if (map[x][y] == '0' or 'M' in map[x][y]):
-                        continue
-                    self.__agent_map[x][y].append('-1')
-                    return self.__coordinate.set(x, y)
-                elif (step == 1):
-                    x = x - move
-                    if (map[x][y] == '0' or 'M' in map[x][y]):
-                        continue
-                    self.__agent_map[x][y].append('-1')
-                    return self.__coordinate.set(x, y)
-                elif (step == 2):
-                    y = y + move
-                    if (map[x][y] == '0' or 'M' in map[x][y]):
-                        continue
-                    self.__agent_map[x][y].append('-1')
-                    return self.__coordinate.set(x, y)
-                elif (step == 3):
-                    y = y - move
-                    if (map[x][y] == '0' or 'M' in map[x][y]):
-                        continue
-                    self.__agent_map[x][y].append('-1')
-                    return self.__coordinate.set(x, y)
-        elif (random_action == 3): # stay and perform large scan
+            x += move
+            current_count = self.__calculate_point(x, y)
+            if x < 0 or y < 0 or x > self.__map.get_height() - 1 or y > self.__map.get_width() - 1 or 'P' in map[x][y] or 'M' in map[x][y] or map[x][y] == '0':
+                break
+            if (current_count>max_count):
+                step = move
+                max_count = current_count
+                recored_dir = 2
+
+        for move in range(1, 5):
+            # Move right
             x = self.__coordinate.get_x()
             y = self.__coordinate.get_y()
-            if ('T' in map[x - 1][y - 1]):
-                return self.__coordinate.set(x - 1, y - 1)
-            if ('T' in map[x - 1][y]):
-                return self.__coordinate.set(x - 1, y)
-            if ('T' in map[x + 1][y + 1]):
-                return self.__coordinate.set(x + 1, y + 1)
-            if ('T' in map[x][y - 1]):
-                return self.__coordinate.set(x, y - 1)
-            if ('T' in map[x][y + 1]):
-                return self.__coordinate.set(x, y + 1)
-            if ('T' in map[x + 1][y - 1]):
-                return self.__coordinate.set(x + 1, y - 1)
-            if ('T' in map[x + 1][y]):
-                return self.__coordinate.set(x + 1, y)
-            if ('T' in map[x + 1][y + 1]):
-                return self.__coordinate.set(x + 1, y + 1)
-            if ('T' in map[x - 2][y - 2]):
-                return self.__coordinate.set(x - 2, y - 2)
-            if ('T' in map[x - 2][y - 1]):
-                return self.__coordinate.set(x - 2, y - 1)
-            if ('T' in map[x - 2][y]):
-                return self.__coordinate.set(x - 2, y)
-            if ('T' in map[x - 2][y + 1]):
-                return self.__coordinate.set(x - 2, y + 1)
-            if ('T' in map[x - 2][y + 2]):
-                return self.__coordinate.set(x - 2, y + 2)
-            if ('T' in map[x - 1][y - 2]):
-                return self.__coordinate.set(x - 1, y - 2)
-            if ('T' in map[x - 1][y + 2]):
-                return self.__coordinate.set(x - 1, y + 2)
-            if ('T' in map[x][y - 2]):
-                return self.__coordinate.set(x + 1, y - 2)
-            if ('T' in map[x][y + 2]):
-                return self.__coordinate.set(x - 1, y + 2)
-            if ('T' in map[x + 1][y - 2]):
-                return self.__coordinate.set(x + 1, y - 2)
-            if ('T' in map[x + 1][y + 2]):
-                return self.__coordinate.set(x + 1, y + 2)
-            if ('T' in map[x + 2][y - 2]):
-                return self.__coordinate.set(x + 2, y - 2)
-            if ('T' in map[x + 2][y - 1]):
-                return self.__coordinate.set(x + 2, y - 1)
-            if ('T' in map[x + 2][y]):
-                return self.__coordinate.set(x + 2, y)
-            if ('T' in map[x + 2][y + 1]):
-                return self.__coordinate.set(x + 2, y + 1)
-            if ('T' in map[x + 2][y + 2]):
-                return self.__coordinate.set(x + 2, y + 2)
-            self.append_map(1, x, y)
-            return self.__coordinate.set(x, y)
+            y += move
+            current_count = self.__calculate_point(x, y)
+            if x < 0 or y < 0 or x > self.__map.get_height() - 1 or y > self.__map.get_width() - 1 or 'P' in map[x][y] or 'M' in map[x][y] or map[x][y] == '0':
+                break
+            if (current_count>max_count):
+                step = move
+                max_count = current_count
+                recored_dir = 3
 
-    def teleport(self, width, height, map):
-        while True:
-            x = random.randint(0, height - 1)
-            y = random.randint(0, width - 1)
-            if ('M' not in map[x][y] and 'T' not in map[x][y] and map[x][y] != '0'):
-                self.__agent_map[x][y].append('-1')
-                return self.__coordinate.set(x, y)
+        for move in range(1, 5):
+            # Move left
+            x = self.__coordinate.get_x()
+            y = self.__coordinate.get_y()
+            y -= move
+            current_count = self.__calculate_point(x, y)
+            if x < 0 or y < 0 or x > self.__map.get_height() - 1 or y > self.__map.get_width() - 1 or 'P' in map[x][y] or 'M' in map[x][y] or map[x][y] == '0':
+                break
+            if (current_count>max_count):
+                step = move
+                max_count = current_count
+                recored_dir = 4
+        if recored_dir == 0:
+            recored_dir = 5
+        return max_count, step, recored_dir
 
-    def append_map(self, num_of_append, x, y): # append -1 to each scanned tile
-        if (num_of_append == 0):
-            self.__agent_map[x][y].append('-1')
-            self.__agent_map[x - 1][y - 1].append('-1')
-            self.__agent_map[x - 1][y].append('-1')
-            self.__agent_map[x - 1][y + 1].append('-1')
-            self.__agent_map[x][y - 1].append('-1')
-            self.__agent_map[x][y + 1].append('-1')
-            self.__agent_map[x + 1][y - 1].append('-1')
-            self.__agent_map[x + 1][y].append('-1')
-            self.__agent_map[x + 1][y + 1].append('-1')
-        if (num_of_append == 1):
-            self.__agent_map[x][y].append('-1')
-            self.__agent_map[x - 1][y - 1].append('-1')
-            self.__agent_map[x - 1][y].append('-1')
-            self.__agent_map[x - 1][y + 1].append('-1')
-            self.__agent_map[x][y - 1].append('-1')
-            self.__agent_map[x][y + 1].append('-1')
-            self.__agent_map[x + 1][y - 1].append('-1')
-            self.__agent_map[x + 1][y].append('-1')
-            self.__agent_map[x + 1][y + 1].append('-1')
+    def action(self):
+        max_count, step, recored_dir = self.__choose_action()
+        if recored_dir == 0:
+            self.__scan(2)
+            print("Stand and scan")
+        elif recored_dir == 1:
+            self.__move(0, step)
+            if step == 1 or step == 2:
+                self.__scan(1)
+            print("Move up", step)
+        elif recored_dir == 2:
+            self.__move(1, step)
+            if step == 1 or step == 2:
+                self.__scan(1)
+            print("Move down", step)
+        elif recored_dir == 3:
+            self.__move(3, step)
+            if step == 1 or step == 2:
+                self.__scan(1)
+            print("Move right", step)
+        elif recored_dir == 4:
+            self.__move(2, step)
+            if step == 1 or step == 2:
+                self.__scan(1)
+            print("Move left", step)
+        elif recored_dir == 5 and self.__teleport == True:
+            print("Teleport :)))")
+            self.teleport()
+            self.__teleport = False
+        else:
+            return False
+        return True
 
-            self.__agent_map[x - 2][y - 2].append('-1')
-            self.__agent_map[x - 2][y - 1].append('-1')
-            self.__agent_map[x - 2][y].append('-1')
-            self.__agent_map[x - 2][y + 2].append('-1')
-            self.__agent_map[x - 2][y + 1].append('-1')
-            self.__agent_map[x - 1][y - 2].append('-1')
-            self.__agent_map[x - 2][y + 2].append('-1')
-            self.__agent_map[x][y - 2].append('-1')
-            self.__agent_map[x][y + 2].append('-1')
-            self.__agent_map[x + 1][y - 2].append('-1')
-            self.__agent_map[x + 1][y + 2].append('-1')
-            self.__agent_map[x + 2][y - 2].append('-1')
-            self.__agent_map[x + 2][y - 1].append('-1')
-            self.__agent_map[x + 2][y].append('-1')
-            self.__agent_map[x + 2][y + 1].append('-1')
-            self.__agent_map[x + 2][y + 2].append('-1')
+        
+
+
+    def teleport(self):
+        map = self.__map.get_data()
+        for x in range(0, self.__map.get_height()):
+            for y in range(0, self.__map.get_width()):
+                current_point = self.__calculate_point(x, y)
+                if current_point > 2:
+                    if ('M' not in map[x][y] and 'T' not in map[x][y] and map[x][y] != '0'):
+                        self.__agent_map[x][y] = '-'
+                        self.__coordinate.set(x, y)
+                        break
+
+    
--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from maps.map import read_input
 from maps.position import Position
 from agent.agent import Agent
+import mapGenerate
 
 if __name__ == '__main__':
-    turn_reveals, turn_free, map_game = read_input('data/Map0.txt')
+    turn_reveals, turn_free, map_game = read_input('data/Map2.txt')
     # TODO: get treasure location
     # map_game.get_treasure().get_x()
     # map_game.get_treasure().get_y()
@@ -18,5 +19,17 @@ if __name__ == '__main__':
     # map_game.get_width()
     # map_game.get_height()
 
-    map = map_game.get_data()
-    print(map[8][13])
+    agent = Agent(map_game)
+    agent.init_map()
+    agent.spawn()
+    # mapGenerate.print_map(agent.get_agent_map())
+    print(agent.get_coordinate().get_x(), agent.get_coordinate().get_y())
+    # agent.action()
+    # print(agent.get_coordinate().get_x(), agent.get_coordinate().get_y())
+    # mapGenerate.print_map(agent.get_agent_map())
+    for i in range(1000):
+        print(i)
+        if agent.action() == False:
+            break
+        print(agent.get_coordinate().get_x(), agent.get_coordinate().get_y())
+        mapGenerate.print_map(agent.get_agent_map())
