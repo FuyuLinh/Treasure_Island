84baeffacba84826e2a9fdd08ceb62253de64f50
tphat2001 <81370495+tphat2001@users.noreply.github.com>
2023-01-07 23:16:50 +0700

    Squashed commit of the following:
    commit dfffa781732ea72224be66b46dc6ea01667cde88
    Author: QuocThieu2002 <pqthieu20@apcs.fitus.edu.vn>
    Date:   Sat Jan 7 23:13:25 2023 +0700
    
        Update pirate.py
    
    commit bb039fb7fb3c77db21ffa1a47ac3ae1f6f7515af
    Author: QuocThieu2002 <pqthieu20@apcs.fitus.edu.vn>
    Date:   Sat Jan 7 22:32:36 2023 +0700
    
        Update hint.py
    
    commit 5bc900862016ae0e45c48dcfc45c6e6d54ad6e2d
    Author: QuocThieu2002 <pqthieu20@apcs.fitus.edu.vn>
    Date:   Sat Jan 7 22:15:32 2023 +0700
    
        Update hint.py
    
    commit 03ee3cee6cf9327e18f40094265978dd075fa7ae
    Author: QuocThieu2002 <pqthieu20@apcs.fitus.edu.vn>
    Date:   Sat Jan 7 19:56:51 2023 +0700
    
        Update hint.py
    
    commit 7846c09e16e22513fdb30bcbbbb7ce8634cd99b3
    Merge: 5270cc6 f859be9
    Author: QuocThieu2002 <pqthieu20@apcs.fitus.edu.vn>
    Date:   Sat Jan 7 18:32:00 2023 +0700
    
        Merge branch 'main' of https://github.com/FuyuLinh/Treasure_Island
    
    commit f859be9b3ca2c53fdfe35cab740f5c7e0f86a1d4
    Merge: 46c6e86 dbcb83b
    Author: Fuyu Linh <nhnlinh20@apcs.fitus.edu.vn>
    Date:   Sat Jan 7 14:54:52 2023 +0700
    
        Merge branch 'agent'
    
    commit 46c6e86c1b957a0d47eeb447145790d06e86806b
    Merge: 610b955 d906132
    Author: Fuyu Linh <105184937+FuyuLinh@users.noreply.github.com>
    Date:   Fri Jan 6 19:18:02 2023 +0700
    
        Merge pull request #3 from FuyuLinh/UI-game
    
        UI-game
    
    commit d90613273367a6f518ed2dcaf689a0d36e75c70e
    Author: tphat2001 <81370495+tphat2001@users.noreply.github.com>
    Date:   Fri Jan 6 19:16:46 2023 +0700
    
        visual
    
    commit 3e5ceebf2f3c41c988d1d25c5a9ccc03ad4b91be
    Author: tphat2001 <81370495+tphat2001@users.noreply.github.com>
    Date:   Wed Jan 4 00:21:41 2023 +0700
    
        UI-game
    
    commit 5270cc67dda34fb7f1e916e5edaa3a8166fc7ee4
    Merge: e10baba 610b955
    Author: QuocThieu2002 <pqthieu20@apcs.fitus.edu.vn>
    Date:   Sun Jan 1 21:00:31 2023 +0700
    
        Merge branch 'main' of https://github.com/FuyuLinh/Treasure_Island
    
    commit e10baba94e6b2ee7432f3326bbce4373f2b63527
    Author: QuocThieu2002 <pqthieu20@apcs.fitus.edu.vn>
    Date:   Sun Jan 1 21:00:24 2023 +0700
    
        Update hint.py
    
    commit 610b955bbb4ac519fdd640632f7387002f22386f
    Author: tphat2001 <81370495+tphat2001@users.noreply.github.com>
    Date:   Fri Dec 30 15:46:02 2022 +0700
    
        Delete 2022-CS420-Project02-Logical-APCS.pdf
    
    commit 900ea7c4f62dd695b662c046b55bfcb528aa68b4
    Author: tphat2001 <81370495+tphat2001@users.noreply.github.com>
    Date:   Fri Dec 30 15:41:54 2022 +0700
    
        Squashed commit of the following:
    
        commit 63af2e93f413243b69cce0a2733668e7383d4ace
        Author: tphat2001 <81370495+tphat2001@users.noreply.github.com>
        Date:   Fri Dec 30 01:36:47 2022 +0700
    
            refix generate
    
        commit a26811ce14501773a013c5b9455f4b396990779c
        Author: tphat2001 <81370495+tphat2001@users.noreply.github.com>
        Date:   Mon Dec 19 14:12:44 2022 +0700
    
            generate 5 requirement maps
    
        commit 79e0e50bd5c3cef092c41cd189072953f3dc22bd
        Author: tphat2001 <81370495+tphat2001@users.noreply.github.com>
        Date:   Mon Dec 19 14:11:05 2022 +0700
    
            implement generate map

--- pirate.py
+++ pirate.py
@@ -1,12 +1,76 @@
 from maps.map import Map
 from maps.position import Position
+import math
+import random
 
 
 class Pirate:
     __position = Position(None, None)
     __map = Map(None, None, None, None, None)
-  
 
 
+    def __init__(self, map) :
+      self.__map = map
 
 
+    def move(self, treasure_X, treasure_Y, main_map, width, height):
+      top, bottom, left, right = False
+      value_top = -1
+      value_bottom = -1
+      value_left = -1
+      value_right = -1
+
+      if self.__X > 0 and 'M' not in str(main_map[self.__X - 1][self.__Y]) and '0' not in str(main_map[self.__X - 1][self.__Y]):
+        top = True
+      if self.__X < width - 1 and 'M' not in str(main_map[self.__X + 1][self.__Y]) and '0' not in str(main_map[self.__X + 1][self.__Y]):
+        bottom = True
+      if self.__Y > 0 and 'M' not in str(main_map[self.__X][self.__Y - 1]) and '0' not in str(main_map[self.__X][self.__Y - 1]):
+        left = True
+      if self.__Y < height - 1 and 'M' not in str(main_map[self.__X][self.__Y + 1]) and '0' not in str(main_map[self.__X][self.__Y + 1]):
+        right = True
+
+      temp_list = []
+      if top == True:
+        value_top = math.sqrt(abs(treasure_X - self.__X - 1)**2 + abs(treasure_Y - self.__Y)**2)
+        temp_list.append(value_top)
+      if bottom == True:
+        value_bottom = math.sqrt(abs(treasure_X - self.__X + 1)**2 + abs(treasure_Y - self.__Y)**2)
+        temp_list.append(value_bottom)
+      if left == True:
+        value_left = math.sqrt(abs(treasure_X - self.__X)**2 + abs(treasure_Y - self.__Y - 1)**2)
+        temp_list.append(value_left)
+      if right == True:
+        value_right = math.sqrt(abs(treasure_X - self.__X)**2 + abs(treasure_Y - self.__Y + 1)**2)
+        temp_list.append(value_right)
+
+      if value_top == min(temp_list):
+        self.__X = self.__X - 1
+        message = 'Pirate go to the top'
+      elif value_bottom == min(temp_list):
+        self.__X = self.__X + 1
+        message = 'Pirate go to the bottom'
+      elif value_left == min(temp_list):
+        self.__Y = self.__Y - 1
+        message = 'Pirate go to the left'
+      elif value_right == min(temp_list):
+        self.__Y = self.__Y + 1
+        message = 'Pirate go to the right'
+
+      return message
+
+    def spawn(self):
+      temp_list_X = []
+      temp_list_Y = []
+
+      for i in range(0, self.__map.):
+        for j in range(0, height):
+          if 'M' in str(main_map[i][j]):
+            temp_list_X.append(i)
+            temp_list_Y.append(j)
+
+      spawn_random = random.randint(0, len(temp_list_X) - 1)
+
+      self.__X = temp_list_X(spawn_random)
+      self.__Y = temp_list_Y(spawn_random)
+
+    
